da797bc9187bdcf3abb0c176dfc34b09d064ad84
Vinayak Joshi <vinayakjoshi2004@gmail.com>
2025-07-12 19:17:23 +0530

    Updated app.py and added requirements.txt for deployment

--- app.py
+++ app.py
@@ -28,5 +28,8 @@ def predict():
     except Exception as e:
         return render_template('index.html', prediction_text="Error: " + str(e))
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
